4beb55d444d9a0ee23a990291f50520b69d4e2e9
Dung Nguyen <contact@dungnt.net>
2021-06-20 23:47:38 +0700

    add pdfRenderOptions

--- index.js
+++ index.js
@@ -41,4 +41,15 @@ function render(resume) {
   });
 }
 
-module.exports = { render };
+const marginValue = '0.8 cm';
+const pdfRenderOptions = {
+  format: 'A4',
+  margin: {
+    top: marginValue,
+    bottom: marginValue,
+    left: marginValue,
+    right: marginValue,
+  }
+}
+
+module.exports = { render, pdfRenderOptions };
